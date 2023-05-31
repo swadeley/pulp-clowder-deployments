9a61280efc5481dcfa022a2419e4477500fd995a
Mike DePaulo <mikedep333@redhat.com>
2023-05-31 09:00:56 -0400

    Initial version of deployment scripts
    [noissue]

---
+++ pr_check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+APP_NAME="ingress"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="ingress"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/pulp/pulp-rpm"  
+
+IQE_PLUGINS="ingress"
+IQE_MARKER_EXPRESSION="smoke"
+IQE_FILTER_EXPRESSION=""
+IQE_CJI_TIMEOUT="30m"
+
+EXTRA_DEPLOY_ARGS="advisor"
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+source $CICD_ROOT/build.sh
+source $APP_ROOT/unit_test.sh
+source $CICD_ROOT/deploy_ephemeral_env.sh
+source $CICD_ROOT/cji_smoke_test.sh
+source $CICD_ROOT/post_test_results.sh
